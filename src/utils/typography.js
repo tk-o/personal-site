340942d7058161f729a350db243f4bfa4410483a
Tomasz Kopacki <tomasz@kopacki.net>
2019-12-16 18:53:52 +1100

    learn more on topic post release

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -3,6 +3,10 @@ import Wordpress2016 from 'typography-theme-wordpress-2016'
 
 Wordpress2016.overrideThemeStyles = () => {
   return {
+    '.highlight': {
+      backgroundColor: '#a4ffd9',//'#5ee2bd',
+      padding: '0.125rem 0',
+    },
     'a.gatsby-resp-image-link': {
       boxShadow: `none`,
     },
@@ -12,6 +16,9 @@ Wordpress2016.overrideThemeStyles = () => {
     'main p': {
       textAlign: `justify`,
     },
+    '.gatsby-resp-image-wrapper + p': {
+      marginTop: '2rem',
+    }
   }
 }
 
